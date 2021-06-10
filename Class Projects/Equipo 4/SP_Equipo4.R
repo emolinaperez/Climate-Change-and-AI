ad2f8cc171f4bdc24712497f98da3b2bb9a8f719
MarianaMonrooy <a01653716@itesm.mx>
2021-06-09 23:40:57 -0600

    .

--- Class Projects/Equipo 4/SP_Equipo4.R
+++ Class Projects/Equipo 4/SP_Equipo4.R
@@ -1,4 +1,9 @@
 #Migración
+#Librarys
+library(randomForest)
+library(tree)
+library (MASS)
+
 #This script uses different methods to fit a classification tree to our data set
  dir.data<-"/Users/marianamonroy/Desktop/Git Hub/Climate-Change-and-AI/Data/Model/"
  data<-read.csv(paste0(dir.data,"ModelData.csv"))
@@ -29,33 +34,31 @@
 #interest response 
  #English
  # the first quartile is below 2, the second 50% quantile is below 2.046, the third quantile is above 2.00
-  #Spanish
-# el primer cuantil se encuentra por debajo del 2 , el segundo cuantil 50% se encuentra por debajo de 2.046,
-  #el tercer cuantil se encuentra por arriba del 2.00 
+ #Spanish
+ #el primer cuantil se encuentra por debajo del 2 , el segundo cuantil 50% se encuentra por debajo de 2.046,
+ #el tercer cuantil se encuentra por arriba del 2.00 
   
   
  #English:
- #Groups with high quantitative variable and low migration and displacement, which predictors are the
+ #Groups with high quantitative variable and low, action for prevent migration and displacement, which predictors are the
  #best predict?
-  #Spanish:
-#Grupos con variable cuantitativo alto y bajo migración y desplazamiento, que predictores predice mejor?
+ #Spanish:
+ #Grupos con variable cuantitativo alto y bajo, acciones para prevenir migración y desplazamiento, que predictores predice mejor?
   
-# now let's partition the response of interest into two groups
-#focus on the migration and displacement 
+ # now let's partition the response of interest into two groups
+ #focus on the migration and displacement 
  #tresshold based on .10 of the observations that are below that limit
  threshold<-as.numeric(quantile(data[,response],0.10,na.rm=TRUE))
  data$response.binary<-as.factor(ifelse(data[,response]>1.5,"High","Low"))
 
  #English
- #comments
  #treshold is equivalent to 1.5, we are going to generate a binary response, saving it as a factor
  # based on the following function, if the answer of interest is above
  # of this function has high migration.
  
 
- #Spanish
- #Comentarios
- #treshold es equivalente a 1.5, sgeneraremos una respuesta binaria, guardandola como un factor
+ #Español
+ #treshold es equivalente a 1.5, generaremos una respuesta binaria, guardandola como un factor
  #en función de la siguiente función, si la respuesta deeinteres esta por arriba
  #de esta función tiene alta migración.
  
@@ -91,151 +94,43 @@
 
  #English:
  #Policy what do we want to know?
- # What makes a country have migration as a function of all observables as a function of ndc
- # and socioeconomic characteristics (word bank).
+ #What variables make a country take action in the area of migration and displacement caused by climate change?
  
- #It was chosen to analyze the classification model, since we will discriminate which ones affect migration and which ones do not,
- # the validation of this model was done through Random Forest and with linear regression comparison.
- 
- # In addition, two linear classification approaches were carried out by comparing significant variables
- #and possible relationship between them merely to delve further into the information and further understand the behavior
- # of variables. # numeric variables.
+ # It was chosen to analyze the classification model, since we will discriminate which country variables make
+ #to take more action on migration and climate displacement,
+ # the validation of this model was done through Random Forest and a linear classification tree,
+ # In addition, two linear regression models were created with the response variable "migration and displacement"
+ #regreesion which was done with the significant variables thrown by the tree and
+ #the second model the avriables used returned on migration and displacement were only
+ #the ndc variables of the random forest. The above as we seek as an answer which variables
+ #they make countries take action, we do not look for sociodemographic ones. Merely to delve further into the information and further understand the behavior
+ # of variables.
  
  #Español:
  #Policy ¿que queremos saber?
- #Qué hace que un pais tenga migracion en funcion de todos los observables en funcion de ndc 
- #y caracteristicas socioeconomicas world bank.
+ #What variables make a country take action in the area of migration and displacement caused by climate change?
  
- #Se escogió para analizar el modelo clasificacion, pues discriminaremos cuales inciden en migracion y cuales no,
- #la validación de este modelo se hizo a traves de Random Forest y con comparación de regresión lineal 
- 
- #Además, se realizaron dos aproacches de clasificación lineal por comparación de variables significativas
- #y posible relación entre ellas meramente por aondar más en la información y comprender aún más el comportamiento
+ #Se escogió para analizar el modelo clasificacion, pues discriminaremos cuales variables de países hacen
+ #que tomen más acciones con respecto a la migración y el desplazamiento climatico,
+ #la validación de este modelo se hizo a traves de Random Forest y un arbol de clasificación lineal,
+ # además se crearon dos modelos de regresión lineal con variable de respuesta "migración y desplazamiento"
+ #regreesion la cual se hizo con las variables significativas arrojadas por el arbol y
+ #el segundo modelo las avriables utilizadas regresadas sobre migración y desplazamiento fueron solamente
+ #las variablees ndc del random forest. Lo anterior pues buscamos como respuesta que variables
+ #hacen que los paises tomen acción, no buscamos las sociodemograficas. Meramente por aondar más en la información y comprender aún más el comportamiento
  #de las variables.
  
- #========================
- # Approach 1: Classification tree
- #========================
- #install.packages("tree")
- library(tree)
- set.seed (55555)
- train<-sample (1: nrow(data.model )/2)
- data.model.test<-data.model[-train ,]
- response.binary.test<-data.model$response.binary[-train ]
- tree.model <- tree(model,data.model ,subset =train )
- tree.model.pred<-predict(tree.model ,data.model.test ,type ="class")
- table(tree.model.pred ,response.binary.test)
- 
- #English:
- 
- #First arbor the model was trained with half the data
- #train
- #The optimal size of the trees is 3 nodes with a deviation of 2
- 
- #The significant variable is EN-ATM-CO2E.PC (Carbon emissions)
- # are below the 8-09133 level, migration and displacement is at a high level,
- # if not, the variable has a low level of migration ...
- 
- # c1:
- #The significant variable is EN-ATM-CO2E.PC (Carbon emissions)
- # if it is below the 8.09133 level, migration and displacement is at a high level,
- # if not, the variable is low level ... which leads to c2.
- # c2
- # The second significant variable that is triggered by being EN-ATM-CO2E.PC (Carbon emissions) greater than 8.09133, the countries of
- # low migration, they can migrate by SH.DYN.MORT (Mortality rate, under-5 (per 1,000 live births)),
- #if it is above 8.09133 and below 9.2.
- 
- 
- #Confusion matrix
- 
- #Compute confusion matrix extract response.binary column from test and put it in response.binary.test
- #what is the real value of the binary variable in the test set?
- #decision tree with tree function.
 
- 
- #Español:
- #Primer arbor se entrenó el modelo con la mitad de los datos 
- #train 
- #El tamaño óptimo del arbos es de 3 nodos con desviación de 2
- 
- #La variable significativa es EN-ATM-CO2E.PC (Emisiones de carbono)
- #están por debajo del nivel 8-09133, la migración y el desplazameniento es de alto nivel,
- # si no, la variable incide bajo nivel de migración...
- 
- #c1: 
- #La variable significativa es EN-ATM-CO2E.PC (Emisiones de carbono)
- # si está está por debajo del nivel 8.09133, la migración y el desplazameniento es de alto nivel,
- # si no, la variable es de bajo nivel... lo cual conduce a c2.
- #c2
- #La segunda variable significativa que se desencadena al ser EN-ATM-CO2E.PC (Emisiones de carbono) mayor de 8.09133, los países de
- #migración baja, podrán migrar por por SH.DYN.MORT (Mortality rate, under-5 (per 1,000 live births)), 
- #si esta está por arriba de 8.09133 y por  debajo de 9.2.
- 
- 
- #Matriz confusión 
- 
- #computar matriz de confusión extraer columna response.bianry de test y ponerlo en response.bianry.test
- #cual es el valor real de la variable binaria en el conjunto de prueba? 
- #arbol de decision con función tree.
-
- 
- 
- #We can use tree prunning to get a better classification tree
- set.seed (55555)
- cv.data.model<-cv.tree(tree.model ,FUN=prune.misclass )
- cv.data.model
- 
- #English:
- #high is wrong 7 times does not predict migration well
- # how many times is the low category wrong? 5 times
- # So this tree has an error rate of (5 + 5) / 100
- # + -rate error: 0.1
- 
- #Español:
- #high se equivoca 7 veces no predice bien migracion
- #cuantas veces se equivoca la categoria bajo? 5 veces
- # So this tree has an error rate of (5+5)/100
- # +-tasa error: 0.1
- 
- #English: 
- # Before and after pruning the tree we have the same error rate, 80 times the model
- # I correctly classify high and 0 times the model correctly classify low
- # 10 times the model was wrong to classify
- #Remember the test database has half the data: 90 out of 180.
- 
- #Español: 
- #Antes y despues de podar el arbol tenemos la misma tasa de error, 80 veces el modelo
- #clasifico correctamente a high y 0 veces el modelo clasifico correctamente en low
- #10 veces el modelo se equivocó en clasificar
- #se recuerda la base de datos de prueba tiene la mitad de los datos: 90 de 180.
- 
- 
- # dev corresponds to the cross-validation error rate (in our case 2) in this instance, which is then the best tree?
- 
- #we can use prune.misclass() to obtain the best tree
- prune.tree.model <- prune.misclass (tree.model ,best = 2)
- plot(prune.tree.model)
- text(prune.tree.model ,pretty =0)
- 
- #how good is the prune tree ?
- tree.pred<-predict (prune.tree.model , data.model.test ,type ="class")
- table(tree.pred ,response.binary.test)
- 
- # So this tree has an error rate of (8+8)/90
- #0.15 error. It predicts well HIGH.
- 
  #========================
- # Approach 2: Regression tree
+ # Approach 1: Regression tree
  #========================
  
  #single best
- library (MASS)
  set.seed (55555)
  train <- sample (1: nrow(data.model), 100)
  Rtree.data.model <-tree(model,data.model ,subset =train)
  summary (Rtree.data.model)
- #ndc y world bank no dice mucho, 
- #treshold por debajo es... de alta 
+
  #desviacion del arbol: 0.1235
  #Misclassification error rate: 0.03
  plot(Rtree.data.model,col="rosybrown4")
@@ -250,16 +145,38 @@
  # minor error a node, 59.86056
  
  prune.Rtree.data.model <- prune.misclass(Rtree.data.model  ,best =5)
- plot(prune.Rtree.data.model )
+ plot1 <- plot(prune.Rtree.data.model )
  text(prune.Rtree.data.model ,pretty =0)
 
+ #English:
+
  
+ 
+ #Español:
+ #Primer arbor se entrenó el modelo con la mitad de los datos 
+ #train 
+ #El tamaño óptimo del arbos es de 5 nodos con desviación de 59.86056
+ 
+ #La variable significativa es EN-ATM-CO2E.PC (Emisiones de carbono)
+ #están por debajo del nivel 8-09133, la migración y el desplazameniento es de alto nivel,
+ # si no, la variable incide bajo nivel de migración...
+ 
+ #c1: 
+ #La variable significativa es EN-ATM-CO2E.PC (Emisiones de carbono)
+ # si está está por debajo del nivel 8.09133, la migración y el desplazameniento es de alto nivel,
+ # si no, la variable es de bajo nivel... lo cual conduce a c2.
+ #c2
+ #La segunda variable significativa que se desencadena al ser EN-ATM-CO2E.PC (Emisiones de carbono) mayor de 8.09133, los países de
+ #migración baja, podrán migrar por por SH.DYN.MORT (Mortality rate, under-5 (per 1,000 live births)), 
+ #si esta está por arriba de 8.09133 y por  debajo de 9.2.
+
+ 
+
  #========================
- # Approach 3: Random Forest
+ # Approach 2: Random Forest
  #========================
  
 #install.packages("randomForest")
- library(randomForest)
  set.seed (55555)
  rf.data.model <- randomForest(model,
                               data=data.model ,
@@ -271,7 +188,7 @@
 
 #base on many different trees, which are the most important drivers
  importance (rf.data.model)
- varImpPlot (rf.data.model, col="rosybrown4")
+ plot2 <- varImpPlot (rf.data.model, col="rosybrown4")
 
 
  #Type of random forest: classification
@@ -280,7 +197,7 @@
  #OOB estimate of  error rate: 7%
  
  #========================
- # Lineal regression
+ # Lineal regression 1
  #========================
  
  #migration_and_displacement
@@ -293,6 +210,10 @@
  model<-as.formula(paste0(response,"~",paste(predictors,collapse="+")))
  full.model <- lm(migration_and_displacement~SP.POP.TOTL+M_PL7+a_water+a_urban, data = data.model)
  summary(full.model)
+ 
+ #========================
+ # Lineal regression 2
+ #========================
  #estimate full model with variables of Random forest Gini NDC
  ndc.randomf <- lm(migration_and_displacement~m_lulucf + a_water + 
                       m_transport + a_environment + a_health + m_waste + a_agriculture + 
@@ -303,108 +224,6 @@
  summary(ndc.randomf)
  
  
- #load library
- library(leaps)
- 
- #first let's divide the sample into a test and a a train set
- set.seed (55555)
- train <- sample (c(TRUE ,FALSE), nrow(data.model),rep=TRUE)
- test  <- (!train )
- 
- #define maximum length of the model
- max.vars<-8
- 
- #let's do full search
- regfit.best <- regsubsets (model, data.model[train,], nvmax =max.vars,really.big = T) #you can choose how large you want the search to be
- 
- #let's do forward stepwise selection
- regfit.fwd <- regsubsets (model, data.model[train,], nvmax =max.vars,really.big = T, method = "forward") #you can choose how large you want the search to be
- 
- #let's do backard stepwise selection
- regfit.bwd <- regsubsets (model, data.model[train,], nvmax =max.vars,really.big = T, method = "backward") #you can choose how large you want the search to be
- 
- 
- #explore different models
- msize<-8
- coef(regfit.best ,msize)
- coef(regfit.fwd , msize)
- coef(regfit.bwd , msize)
- 
- 
- #now how do we select which model is best
- 
- predict.regsubsets <-function (object, model ,newdata ,id ){
-   #object<-regfit.best
-   #newdata<-data.model[test ,]
-   #id<-1
-   form<-model
-   options(na.action='na.pass')
-   mat<-model.matrix (form,newdata )
-   coefi<-coef(object ,id=id)
-   xvars<-names (coefi )
-   pred<-mat[,xvars ]%*% coefi
-   val.errors<- mean((newdata[,response]-pred)^2,na.rm=TRUE)
-   val.errors
- }
- 
- #now estimate test error for the different versions of the models
- #best subset
- cv.best<-data.frame(subset.type="best",
-                     nvars=1,
-                     test.mse=predict.regsubsets(regfit.best,model,data.model[test ,],1))
- 
- for(i in 2:max.vars)
- {
-   pivot<-data.frame(subset.type="best",
-                     nvars=i,
-                     test.mse=predict.regsubsets(regfit.best,model,data.model[test ,],i))
-   cv.best<-rbind(cv.best,pivot)
-   
- }
- 
- #best model
- subset(cv.best,test.mse==min(test.mse))
- #actual model
- coef(regfit.best ,subset(cv.best,test.mse==min(test.mse))$nvars)
- 
- #forward method
- cv.fwd<-data.frame(subset.type="fwd",
-                    nvars=1,
-                    test.mse=predict.regsubsets(regfit.fwd,model,data.model[test ,],1))
- 
- for(i in 2:max.vars)
- {
-   pivot<-data.frame(subset.type="fwd",
-                     nvars=i,
-                     test.mse=predict.regsubsets(regfit.fwd,model,data.model[test ,],i))
-   cv.fwd<-rbind(cv.fwd,pivot)
-   
- }
- 
- #best model
- subset(cv.fwd,test.mse==min(test.mse))
- #actual model
- coef(regfit.fwd ,subset(cv.fwd,test.mse==min(test.mse))$nvars)
- 
- #
- #backward method
- cv.bwd<-data.frame(subset.type="bwd",
-                    nvars=1,
-                    test.mse=predict.regsubsets(regfit.bwd,model,data.model[test ,],1))
- 
- for(i in 2:max.vars)
- {
-   pivot<-data.frame(subset.type="bwd",
-                     nvars=i,
-                     test.mse=predict.regsubsets(regfit.bwd,model,data.model[test ,],i))
-   cv.bwd<-rbind(cv.bwd,pivot)
-   
- }
- 
- #best model
- subset(cv.bwd,test.mse==min(test.mse))
- #actual model
- coef(regfit.bwd ,subset(cv.bwd,test.mse==min(test.mse))$nvars)
- 
+
  
 
